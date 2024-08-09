391995e2ec9eff5bf15bf696af04545bbf09fbf8
Densaugeo <>
2024-08-08 23:55:05 -0400

    Finally, after far too many complications, got the ridiculously over-convoluted webauthn APIs to complete a login challenge

--- app-fastapi.py
+++ app-fastapi.py
@@ -3,8 +3,9 @@
 import collections, secrets, json, pathlib, os, base64, hashlib
 
 #import requests
-from fastapi import FastAPI, Header, Form, Response, HTTPException, Depends
-from fastapi.responses import PlainTextResponse, HTMLResponse
+from fastapi import FastAPI, Header, Form, Response, HTTPException, Depends, Request
+from fastapi.responses import PlainTextResponse, HTMLResponse, FileResponse, JSONResponse
+import fastapi.responses
 from fastapi.staticfiles import StaticFiles
 from starlette.types import Receive, Scope, Send
 from setproctitle import setproctitle
@@ -31,7 +32,7 @@ class StaticFilesCustom(helpers.StaticFilesWithDir):
         
         if scope['path'].startswith('/static/restricted/') \
         and not likely_vrc_client:
-            raise HTTPException(status_code=403)
+            raise HTTPException(401, 'Unauthorized - login at https:/login.html')
         
         await super().__call__(scope, receive, send)
 
@@ -68,10 +69,92 @@ async def get_echo(number: int, string: Annotated[str, Query(max_length=4)]):
 
 
 from fastapi.security import OAuth2PasswordBearer
+import json
+from webauthn import verify_authentication_response, base64url_to_bytes, verify_registration_response
+
+challenge = None
+allow_credentials = [
+    'iJdDqCOfx8Fkh+6PJHsw2sI98RevmuQ2UXiMe2gUgEQHTIVYpIUxvWoX6izv6M2IHXFp5IVXj6PSXAB+ephfog==',
+]
+public_keys = [
+    #bytes([48, 89, 48, 19, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 8, 42, 134, 72, 206, 61, 3, 1, 7, 3, 66, 0, 4, 213, 234, 207, 60, 174, 205, 100, 74, 105, 96, 227, 16, 254, 247, 215, 131, 195, 28, 29, 245, 38, 9, 61, 170, 223, 102, 211, 100, 71, 50, 33, 95, 206, 89, 85, 124, 248, 12, 138, 196, 77, 188, 60, 177, 222, 51, 232, 71, 203, 41, 203, 225, 231, 5, 185, 35, 175, 179, 142, 206, 200, 11, 86, 189]),
+    'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1erPPK7NZEppYOMQ/vfXg8McHfUmCT2q32bTZEcyIV/OWVV8+AyKxE28PLHeM+hHyynL4ecFuSOvs47OyAtWvQ',
+]
+
+@app.get('/login.html')
+async def get_login_html():
+    return FileResponse('login.html')
+
+@app.post('/prelogin')
+async def post_prelogin():
+    global challenge
+    challenge = [1, 2, 3, 4, 5, 6, 7, 8]
+    return JSONResponse({
+        'challenge': challenge,
+        'allowCredentials': allow_credentials,
+    })
+
+@app.post('/login')
+async def post_login(request: Request):
+    global challenge
+    if challenge is None:
+        raise HTTPException(403, 'Forbidden - Challenge not set')
+    challenge_local = challenge
+    challenge = None
+    
+    body = json.loads(await request.body())
+    print(body)
+    
+    credential = """{{
+        "id": "{}",
+        "rawId": "{}",
+        "response": {{
+            "authenticatorData": "{}",
+            "clientDataJSON": "{}",
+            "signature": "{}",
+            "userHandle": "{}"
+        }},
+        "type": "public-key",
+        "authenticatorAttachment": "cross-platform",
+        "clientExtensionResults": {{}}
+    }}""".format(allow_credentials[0], allow_credentials[0], body['authenticatorData'], body['clientDataJSON'], body['signature'], 'den_antares')
+    
+    print()
+    print(credential)
+    #print(base64url_to_bytes(public_keys[0]))
+    print()
+    print(json.loads(body['create_string']))
+    print()
+    
+    print('Attempting verify_registration_response()')
+    verified_registration = verify_registration_response(
+        credential=json.loads(body['create_string']),
+        expected_challenge=bytes([117, 61, 252, 231, 191, 241, 7, 8, 117, 61, 252, 231, 191, 241, 7, 8]),
+        expected_rp_id='localhost',
+        expected_origin='https://localhost:8443',
+        require_user_verification=False,
+    )
+    print(verified_registration)
+    print()
+    
+    verified_response = verify_authentication_response(
+        credential=body['json_dump'],
+        expected_challenge=bytes(challenge_local),
+        expected_rp_id='localhost',
+        expected_origin='https://localhost:8443',
+        credential_public_key=verified_registration.credential_public_key,
+        credential_current_sign_count=0,
+        require_user_verification=False,
+    )
+    print('Finally got a verified response:')
+    print(verified_response)
+    print()
+    
+    return PlainTextResponse('some-token')
 
 def validate_token(token: str):
     if token != 'some-token':
-        raise HTTPException(401, 'Invalid token')
+        raise HTTPException(401, 'Unauthorized - Invalid token')
     
     return token
 
@@ -79,7 +162,3 @@ def validate_token(token: str):
 async def get_restricted(token: Annotated[str, Depends(validate_token)]):
     print(f'Got token: {token}')
     return 'hi'
-
-@app.post('/login')
-async def post_login():
-    return PlainTextResponse('some-token')
--- passkey-notes.md
+++ passkey-notes.md
@@ -1,6 +1,7 @@
 Create a non-resident key:
 
 ```
+// In Firefox, this cannot be run while the browser console is open
 credential = await navigator.credentials.create({
   publicKey: {
     // MDN states the challenge should be at least 16 bytes
@@ -12,13 +13,30 @@ credential = await navigator.credentials.create({
       name: "den_antares",
       displayName: "Den Antares"
     },
-    pubKeyCredParams: [ {type: "public-key", alg: -7} ],
+    pubKeyCredParams: [
+      { type: "public-key", alg: -7 },
+      { type: "public-key", alg: -257 },
+    ],
   }
 });
 ```
 
-credential.id // 'Y4CNZMvxzgqhDXxgJrjdPR5DlRPZ9nilCNJYNsypNcvcPLkg_FgMnlbnXqKkJErUnKpHnyF1fH6a7WNFo7cv4Q' - looks like a public key, though I haven't found any confirmation for that
-credential.response // Appears to be an optional thing for 'attestation'
+
+
+credential.id // 'iJdDqCOfx8Fkh-6PJHsw2sI98RevmuQ2UXiMe2gUgEQHTIVYpIUxvWoX6izv6M2IHXFp5IVXj6PSXAB-ephfog'
+buffer_to_b64(credential.rawId) // 'iJdDqCOfx8Fkh+6PJHsw2sI98RevmuQ2UXiMe2gUgEQHTIVYpIUxvWoX6izv6M2IHXFp5IVXj6PSXAB+ephfog=='
+new Uint8Array(credential.response.getPublicKey()) // [48, 89, 48, 19, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 8, 42, 134, 72, 206, 61, 3, 1, 7, 3, 66, 0, 4, 175, 66, 227, 211, 220, 207, 44, 137, 22, 146, 96, 237, 70, 69, 38, 176, 161, 126, 213, 144, 161, 203, 213, 34, 133, 154, 66, 213, 250, 59, 14, 173, 39, 236, 42, 109, 100, 176, 15, 41, 246, 194, 225, 108, 232, 212, 248, 155, 222, 246, 225, 202, 228, 209, 31, 149, 89, 94, 69, 128, 141, 118, 19, 216]
+buffer_to_b64(credential.response.getPublicKey()) // 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEr0Lj09zPLIkWkmDtRkUmsKF+1ZChy9UihZpC1fo7Dq0n7CptZLAPKfbC4Wzo1Pib3vbhyuTRH5VZXkWAjXYT2A=='
+
+JSON.stringify({
+  id: credential.id,
+  rawId: buffer_to_b64(credential.rawId),
+  response: {
+    attestationObject: buffer_to_b64(credential.response.attestationObject),
+    clientDataJSON: buffer_to_b64(credential.response.clientDataJSON),
+  },
+  type: credential.type,
+}) // '{"id":"iJdDqCOfx8Fkh-6PJHsw2sI98RevmuQ2UXiMe2gUgEQHTIVYpIUxvWoX6izv6M2IHXFp5IVXj6PSXAB-ephfog","rawId":"iJdDqCOfx8Fkh+6PJHsw2sI98RevmuQ2UXiMe2gUgEQHTIVYpIUxvWoX6izv6M2IHXFp5IVXj6PSXAB+ephfog==","response":{"attestationObject":"o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVjESZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NBAAAAAQAAAAAAAAAAAAAAAAAAAAAAQIiXQ6gjn8fBZIfujyR7MNrCPfEXr5rkNlF4jHtoFIBEB0yFWKSFMb1qF+os7+jNiB1xaeSFV4+j0lwAfnqYX6KlAQIDJiABIVggr0Lj09zPLIkWkmDtRkUmsKF+1ZChy9UihZpC1fo7Dq0iWCAn7CptZLAPKfbC4Wzo1Pib3vbhyuTRH5VZXkWAjXYT2A==","clientDataJSON":"eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiZFQzODU3X3hCd2gxUGZ6bnZfRUhDQSIsIm9yaWdpbiI6Imh0dHBzOi8vbG9jYWxob3N0Ojg0NDMiLCJjcm9zc09yaWdpbiI6ZmFsc2V9"},"type":"public-key"}'
 
 Authenticate with non-resident key:
 
@@ -36,29 +54,6 @@ login_response = await navigator.credentials.get({
       id: new Uint8Array([99, 128, 141, 100, 203, 241, 206, 10, 161, 13, 124, 96, 38, 184, 221, 61, 30, 67, 149, 19, 217, 246, 120, 165, 8, 210, 88, 54, 204, 169, 53, 203, 220, 60, 185, 32, 252, 88, 12, 158, 86, 231, 94, 162, 164, 36, 74, 212, 156, 170, 71, 159, 33, 117, 124, 126, 154, 237, 99, 69, 163, 183, 47, 225]),
       //id: new TextEncoder().encode(credential.id),
     }],
-  }
-});
-```
-
-The resulting login_response.response.authenticatorData and login_response.response.signature remain the same for repeated calls. However,
-varying the challenge changes login_response.response.signature
-
-Create a resident key:
-
-```
-credential_2 = await navigator.credentials.create({
-  publicKey: {
-    challenge: new Uint8Array([117, 61, 252, 231, 191, 241]),
-    rp: { id: "mozilla.org", name: "ACME Corporation" },
-    user: {
-      id: new Uint8Array([79, 252, 83, 72, 214, 7, 89, 26]),
-      name: "jamiedoe",
-      displayName: "Jamie Doe"
-    },
-    pubKeyCredParams: [ {type: "public-key", alg: -7} ],
-    authenticatorSelection: {
-      residentKey: 'required',
-    },
   }
 });
 ```
